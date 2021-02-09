08bf7c5820f643438482a9ef0538e478af670643
Alqahtani <sarra-alqahtani@wfu.edu>
2021-02-09 09:03:50 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class Main {
+
+    // open file and construct a file reader
+    static public Scanner openInputFile(){
+        System.out.println("Fix me");
+        return null;
+    }
+
+    //print the letters in caps
+    static public void processLine(String line){
+
+    }
+
+    // read each line of file and pass to processLine method to print the captalize the letters
+    static public void processFile(Scanner bf){
+
+    }
+
+    public static void main(String[] args) {
+        // Choose a text file lcated in .src folder
+       String FILENAME = "MobyDick";
+       Scanner fileReader= openInputFile();
+       processFile(fileReader);
+           }
+}
